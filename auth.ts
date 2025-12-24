120ad98d6f88b29c6c167ba5cdb4228726659160
Bhupesh Dahiya <149587292+basesixtyfour@users.noreply.github.com>
2025-12-24 16:36:16 +0530

    Add Auth

---
+++ auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { jwt } from "better-auth/plugins";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+  basePath: "/api/auth",
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+  plugins: [jwt()],
+});
